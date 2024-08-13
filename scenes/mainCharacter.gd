dc4d7002380b5e47567f7882ff384998727eb21b
Ivan Villagra Latorre <78452823+ivanvillagra@users.noreply.github.com>
2024-08-14 01:58:18 +0200

    fixear

--- scenes/mainCharacter.gd
+++ scenes/mainCharacter.gd
@@ -9,18 +9,19 @@ var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 @onready var animation = $AnimatedSprite2D
 
 func _physics_process(delta):
-	_idle()
-	
-	_jump(delta)
-	
-	_doubleJump()
-	
-	_downDash()
-	
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	_walk()
 
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+		doubleJump = true
+		
+	if not is_on_floor():
+		velocity.y += gravity * delta
+		animation.play("jump")
+		
+	_doubleJump()
+	_downDash()
+	_walk()
+	_attack()
 	move_and_slide()
 
 func _doubleJump():
@@ -48,19 +49,12 @@ func _walk():
 			animation.play("walk")
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
+		if is_on_floor():
+			animation.play("idle2")
 		
-func _idle():
-	if velocity.length() == 0 && is_on_floor():
-		animation.animation_finished
-		animation.play("idle2")
+
+func _attack():
+	if Input.is_action_just_pressed("ui_up") and is_on_floor():
+		animation.play("attack1")
 		
-func _jump(delta):
-		# Add the gravity.
-	if not is_on_floor():
-		animation.animation_finished
-		animation.play("jump")
-		velocity.y += gravity * delta
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-		doubleJump = true
+	

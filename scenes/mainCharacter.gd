83657ba9f08de98a657c09197b9a01db01ac821b
Ivan Villagra Latorre <78452823+ivanvillagra@users.noreply.github.com>
2024-08-14 00:14:53 +0200

    all animations
    add all animations

--- scenes/mainCharacter.gd
+++ scenes/mainCharacter.gd
@@ -2,7 +2,7 @@ extends CharacterBody2D
 
 
 const SPEED = 300.0
-const JUMP_VELOCITY = -600.0
+const JUMP_VELOCITY = -500.0
 var doubleJump:bool = true
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")

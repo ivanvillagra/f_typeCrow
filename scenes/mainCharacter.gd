7b3709fe3431352bca9599e308952a1bcf3136f8
Ivan Villagra Latorre <78452823+ivanvillagra@users.noreply.github.com>
2024-08-15 20:22:10 +0200

    first animation attack correct

--- scenes/mainCharacter.gd
+++ scenes/mainCharacter.gd
@@ -3,18 +3,19 @@ extends CharacterBody2D
 const SPEED = 300.0
 const JUMP_VELOCITY = -500.0
 var double_jump_enabled: bool = true
+var attacking: bool = false
 
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 @onready var animation = $AnimatedSprite2D
 
 func _ready():
-	# Conectar la señal de animación terminada
-	animation.animation_looped.connect(_on_animation_finished)
+	animation.animation_finished.connect(_on_animation_finished)
 
 func _physics_process(delta):
 	_handle_gravity(delta)
 	_handle_movement(delta)
-
+	move_and_slide()
+	
 func _handle_gravity(delta):
 	if not is_on_floor():
 		velocity.y += gravity * delta
@@ -22,28 +23,28 @@ func _handle_gravity(delta):
 			animation.play("jump")
 
 func _handle_movement(delta):
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor() and !attacking:
 		_jump()
 	elif Input.is_action_just_pressed("ui_accept") and !is_on_floor() and double_jump_enabled:
 		_double_jump()
 	elif Input.is_action_just_pressed("ui_down") and !is_on_floor():
 		_down_dash()
+	elif Input.is_action_just_pressed("ui_down") and is_on_floor():
+		_attack() 
 
-	var direction = Input.get_axis("ui_left", "ui_right")
-	if direction != 0:
-		velocity.x = direction * SPEED
-		animation.flip_h = velocity.x < 0
+	if !attacking:
+		var direction = Input.get_axis("ui_left", "ui_right")
+		if direction != 0:
+			velocity.x = direction * SPEED
+			animation.flip_h = velocity.x < 0
 
-		if is_on_floor() and animation.get_animation() != "attack":
-			animation.play("walk")
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		if Input.is_action_just_pressed("ui_down") and is_on_floor():
-			_attack()
-		elif is_on_floor() and animation.get_animation() != "attack":
-			animation.play("idle2")
-	
-	move_and_slide()
+			if is_on_floor() and animation.get_animation() != "attack":
+				animation.play("walk")
+		else:
+			velocity.x = move_toward(velocity.x, 0, SPEED)
+			if is_on_floor() and !attacking:
+				animation.play("idle2")
+
 
 func _jump():
 	velocity.y = JUMP_VELOCITY
@@ -59,14 +60,13 @@ func _double_jump():
 
 func _down_dash():
 	velocity.y = -(JUMP_VELOCITY * 2.25)
-	if animation.get_animation() != "fall":
-		animation.play("fall")
 
 func _attack():
-	if animation.get_animation() != "attack1":
-		animation.play("attack1")
-
+	attacking = true
+	velocity.x = move_toward(velocity.x, 0, SPEED)
+	animation.play("attack1")
+		
 func _on_animation_finished():
-	if animation.get_animation() == "attack1":
-		if is_on_floor():
-			animation.play("idle2")
+	if  is_on_floor():
+			attacking = false
+	

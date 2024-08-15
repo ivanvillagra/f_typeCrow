28224c4e3491118718394e6359cb1c48dc5a2ecd
ivanvillagra <78452823+ivanvillagra@users.noreply.github.com>
2024-08-15 22:33:28 +0200

    animation attacks basics

--- scripts/mainCharacter.gd
+++ scripts/mainCharacter.gd
@@ -4,6 +4,7 @@ const SPEED = 300.0
 const JUMP_VELOCITY = -500.0
 var double_jump_enabled: bool = true
 var attacking: bool = false
+var life = 100
 
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 @onready var animation = $AnimatedSprite2D
@@ -19,8 +20,13 @@ func _physics_process(delta):
 func _handle_gravity(delta):
 	if not is_on_floor():
 		velocity.y += gravity * delta
-		if animation.get_animation() != "jump" and animation.get_animation() != "fall":
+		if animation.get_animation() != "jump" and !attacking:
 			animation.play("jump")
+	
+
+	if animation.get_animation() == "attack4" and is_on_floor():
+		attacking = false
+		
 
 func _handle_movement():
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor() and !attacking:
@@ -31,6 +37,8 @@ func _handle_movement():
 		_down_dash()
 	elif Input.is_action_just_pressed("ui_down") and is_on_floor():
 		_attack() 
+	elif Input.is_action_just_pressed("ui_down") and !is_on_floor():
+		air_attack()
 
 	if !attacking:
 		var direction = Input.get_axis("ui_left", "ui_right")
@@ -49,26 +57,26 @@ func _handle_movement():
 func _jump():
 	velocity.y = JUMP_VELOCITY
 	double_jump_enabled = true
-	if animation.get_animation() != "jump":
-		animation.play("jump")
 
 func _double_jump():
-	velocity.y = JUMP_VELOCITY
+	velocity.y = JUMP_VELOCITY * 1.25
 	double_jump_enabled = false
-	if animation.get_animation() != "jump":
-		animation.play("jump")
+
 
 func _down_dash():
 	attacking = true
-	velocity.y = -(JUMP_VELOCITY * 1.75)
+	velocity.y = -(JUMP_VELOCITY * 1.25)
 	animation.play("attack4")
 
 func _attack():
 	attacking = true
 	velocity.x = move_toward(velocity.x, 0, SPEED)
 	animation.play("attack1")
+
+func air_attack():
+	attacking = true
+	animation.play("attack2")
 		
 func _on_animation_finished():
-	if is_on_floor():
-		attacking = false
+	attacking = false
 	

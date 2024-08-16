cde2e81a48e792e92dd882c1c9877091159995f7
ivanvillagra <78452823+ivanvillagra@users.noreply.github.com>
2024-08-16 02:09:13 +0200

    errores

--- scripts/mainCharacter.gd
+++ scripts/mainCharacter.gd
@@ -6,6 +6,7 @@ var double_jump_enabled: bool = true
 var attacking: bool = false
 var life = 100
 var attacks_damage = 10
+const knockback_force = 15
 
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 @onready var animation = $AnimatedSprite2D
@@ -16,44 +17,40 @@ func _ready():
 func _physics_process(delta):
 	_handle_gravity(delta)
 	_handle_movement()
+	_collisions()
 	move_and_slide()
-	
+
 func _handle_gravity(delta):
 	if not is_on_floor():
 		velocity.y += gravity * delta
-		if animation.get_animation() != "jump" and !attacking:
+		if animation.get_animation() != "jump" and not attacking:
 			animation.play("jump")
-	
-
-	if animation.get_animation() == "attack4" and is_on_floor():
+	elif animation.get_animation() == "attack4":
 		attacking = false
-		
 
 func _handle_movement():
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor() and !attacking:
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor() and not attacking:
 		_jump()
-	elif Input.is_action_just_pressed("ui_accept") and !is_on_floor() and double_jump_enabled:
+	elif Input.is_action_just_pressed("ui_accept") and not is_on_floor() and double_jump_enabled:
 		_double_jump()
-	elif Input.is_action_just_pressed("ui_down") and !is_on_floor() and !double_jump_enabled:
+	elif Input.is_action_just_pressed("ui_down") and not is_on_floor() and not double_jump_enabled:
 		_down_dash()
 	elif Input.is_action_just_pressed("ui_down") and is_on_floor():
-		_attack() 
-	elif Input.is_action_just_pressed("ui_down") and !is_on_floor():
+		_attack()
+	elif Input.is_action_just_pressed("ui_down") and not is_on_floor():
 		air_attack()
 
-	if !attacking:
+	if not attacking:
 		var direction = Input.get_axis("ui_left", "ui_right")
 		if direction != 0:
 			velocity.x = direction * SPEED
 			animation.flip_h = velocity.x < 0
-
 			if is_on_floor() and animation.get_animation() != "attack":
 				animation.play("walk")
 		else:
 			velocity.x = move_toward(velocity.x, 0, SPEED)
-			if is_on_floor() and !attacking:
+			if is_on_floor() and not attacking:
 				animation.play("idle2")
-
 
 func _jump():
 	velocity.y = JUMP_VELOCITY
@@ -62,7 +59,6 @@ func _jump():
 func _double_jump():
 	velocity.y = JUMP_VELOCITY * 1.25
 	double_jump_enabled = false
-
 
 func _down_dash():
 	attacking = true
@@ -77,7 +73,13 @@ func _attack():
 func air_attack():
 	attacking = true
 	animation.play("attack3")
-		
+
 func _on_animation_finished():
 	attacking = false
-	
+
+func _collisions():
+	if get_slide_collision_count() > 0:
+		for i in range(get_slide_collision_count()):  # Corregido: use `range` para iterar sobre índices
+			var collision = get_slide_collision(i)
+			if collision and collision.get_collider().name == "Enemy":
+				velocity.x -= (collision.get_position().x - global_position.x) * knockback_force
